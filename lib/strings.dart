d7d502bd4e04234d51b5226efbad54ef24534834
Deepak Kishore S <sdkdeepak600@gmail.com>
2023-04-19 22:30:58 +0530

    login button

--- lib/strings.dart
+++ lib/strings.dart
@@ -1,11 +1,12 @@
 const emailOrPasswordEmptyDialogTitle =
-'Please fill in both email and the password fields';
+    'Please fill in both email and the password fields';
 const emailOrPasswordEmptyDescription =
-'You seem to have forgotten to enter either the email or the password fields';
+    'You seem to have forgotten to enter either the email or the password fields';
 const loginErrorDialogTitle = 'Login error';
 const loginErrorDialogContent =
-'Invalid email/password combination. Please try again with valid login credentials';
+    'Invalid email/password combination. Please try again with valid login credentials';
 const pleaseWait = 'Please wait...';
 const enterYourPasswordHere = 'Enter your password here...';
 const enterYourEmailHere = 'Enter your email here...';
 const ok = 'OK';
+const String login = "Login";
---
+++ lib/views/login_button.dart
@@ -0,0 +1,38 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc_examples/dialog/generic_dialog.dart';
+
+import '../strings.dart';
+
+typedef OnLogInTapped = void Function(String email, String password);
+
+class LoginButton extends StatelessWidget {
+  final TextEditingController emailController;
+  final TextEditingController passwordController;
+  final OnLogInTapped onLoginTapped;
+
+  const LoginButton({
+    Key? key,
+    required this.emailController,
+    required this.passwordController,
+    required this.onLoginTapped,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+        onPressed: () {
+          final email = emailController.text;
+          final password = passwordController.text;
+
+          if (email.isEmpty || password.isEmpty) {
+            showGenericDialog(
+                context: context,
+                title: emailOrPasswordEmptyDialogTitle,
+                content: emailOrPasswordEmptyDescription,
+                optionDialogBuilder: () => {"ok": true});
+          }
+        },
+        child: const Text(login));
+  }
+}
