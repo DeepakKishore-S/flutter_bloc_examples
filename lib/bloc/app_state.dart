1f191e4aeb7fefa435e92dfdd5b4ac3eeead021c
zigainfotechmobileprojects <zigainfotechmobileprojects@gmail.com>
2023-04-24 10:01:04 +0530

    updated

--- lib/bloc/app_state.dart
+++ lib/bloc/app_state.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/foundation.dart' show immutable;
 import 'package:flutter_bloc_examples/model.dart';
+import 'package:collection/collection.dart';
 
 @immutable
 class AppState {
@@ -28,4 +29,27 @@ class AppState {
         "loginHandle": loginHandle,
         "fetchedNotes": fetchedNotes,
       }.toString();
+
+  @override
+  bool operator ==(covariant AppState other) {
+    final otherPropertiesAreEqual = other.isLoading == isLoading &&
+        other.loginError == loginError &&
+        other.loginHandle == loginHandle;
+
+    if (fetchedNotes == null && other.fetchedNotes == null) {
+      return true;
+    } else {
+      return otherPropertiesAreEqual &&
+          (fetchedNotes?.isEqualto(other.fetchedNotes) ?? false);
+    }
+  }
+  
+  @override
+  int get hashCode => Object.hash(isLoading, loginError, loginHandle, fetchedNotes);
+  
+}
+
+extension UnOrderedEquality on Object {
+  bool isEqualto(other) =>
+      const DeepCollectionEquality.unordered().equals(this, other);
 }
